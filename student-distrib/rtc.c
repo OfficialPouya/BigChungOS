782126520f23b9ed7d8445ed8b23b33dd8ce47ee
Chloe Zwolfer <chloezwolfer@Chloes-MacBook-Pro.local>
2020-10-17 17:00:48 -0500

    added handler

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -10,13 +10,23 @@
 *  IMPACTS ON OTHERS: NONE
 */
 void init_rtc(void){
-    outb(REG_A, RTC_PORT); //select register A
+    outb(REG_A_NMI, RTC_PORT); //select register A
     unsigned char prev = inb(CMOS_PORT); //reads current value of register B
-    outb(REG_B, RTC_PORT); // select register B
+    outb(REG_B_NMI, RTC_PORT); // select register B
     outb( prev | 0x40, CMOS_PORT); // ored with 0x40 to turn on Periodic Interrupt Enable
     enable_irq(RTC_IRQ_LINE); //enables IRQ line which is line 8 for RTC
 }
 
-void handle_rtc(){
-
+/* handle_rtc 
+*  DESCRIPTION: handles RTC interrupts 
+*  INPUTS: NONE
+*  OUTPUTS: NONE
+*  IMPACTS ON OTHERS: NONE
+*/ 
+void handle_rtc(void){
+     cli(); 
+     outb(REG_C, RTC_PORT); // select register C
+     inb(CMOS_PORT); // throw away contents
+     send_eoi(RTC_IRQ_LINE); 
+     sti(); 
 }

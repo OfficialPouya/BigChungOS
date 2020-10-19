3233b94c842a0a3bfa7294b741f23139ac5f51e6
pa2 <pa2@illinois.edu>
2020-10-19 10:33:04 -0500

    Delete linkage_asm.h

--- student-distrib/linkage_asm.h
+++
@@ -1,9 +0,0 @@
-#include "x86_desc.h"
-#include "keyboard.h"
-#include "lib.h"
-#include "interrupt_handler.h"
-#include "rtc.h"
-
-extern void keyboard_asm();
-extern void handle_rtc();
-extern void syscall_handler_asm();

502bb9ddb0cca1a35de3360c0be28d7b104095b2
Chloe Zwolfer <chloezwolfer@Chloes-MacBook-Pro.local>
2020-10-23 15:56:17 -0500

    rtc, still needs some comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -1,7 +1,10 @@
 /* rtc.c functions to handle the RTC */
 #include "lib.h"
-#include "rtc.h"
 #include "i8259.h"
+#include "rtc.h"
+
+/* global variables */
+int interrupt_flag = 0; //flag to know whether an interrupt has occured 
 
 /* init_rtc
 *  DESCRIPTION: Initializes the RTC by setting registers and turning on the appropriate IRQ (IRQ #8)
@@ -28,7 +31,106 @@ void handle_rtc(void){
      outb(REG_C, RTC_PORT); // select register C
      inb(CMOS_PORT); // throw away contents
      send_eoi(RTC_IRQ_LINE);
+     interrupt_flag = 1; // interrupt has occurred
      //test_interrupts(); //tests if RTC works properly
-     sti();
-     
+     sti();     
+}
+/* rtc_set_frequency 
+*  DESCRIPTION: helper function to be used by open_rtc and write_rtc to set the frequency of the RTC. 
+*  INPUTS: frequency that the RTC should be set to 
+*  OUTPUTS: NONE
+*  IMPACTS ON OTHERS: sets the frequency passed in by other functions. 
+*/
+void rtc_set_frequency(int32_t frequency){
+    char rate; 
+    unsigned char saved_a; 
+    //frequency should not exceed 1024
+    if(frequency > 1024){
+        frequency = 1024;  
+    }
+    //sets rate according to frequency, rate values taken from RTC document. 
+    if(frequency == 2){
+        rate = 0x0F;
+    }
+    if(frequency == 4){
+        rate = 0x0E;
+    }
+    if(frequency == 8){
+        rate = 0x0D;
+    }
+    if(frequency == 16){
+        rate = 0x0C;
+    }
+    if(frequency == 32){
+        rate = 0x0B;
+    }
+    if(frequency == 64){
+        rate = 0x0A; 
+    }
+    if(frequency == 128){
+        rate = 0x09;
+    }
+    if(frequency == 256){
+        rate = 0x08;
+    }
+    if(frequency == 512){
+        rate = 0x07;
+    }
+    if(frequency == 1024){
+        rate = 0x06;
+    }
+    //sets the frequency of the RTC using the selected rate
+    cli();
+    outb(REG_A_NMI, RTC_PORT); // select register A
+    saved_a = inb(CMOS_PORT); // save the previous value of A 
+    outb(REG_A_NMI, RTC_PORT); // select register A
+    outb( (saved_a & 0xF0) | rate , CMOS_PORT); // saves the upper 4 bits of register A and sets low 4 bits of register A with the selected rate
+    sti();
+}
+/* rtc_open
+*  DESCRIPTION: Opens the RTC and sets the RTC frequency to 2 and returns 0. 
+*  INPUTS: pointer to file
+*  OUTPUTS: NONE
+*  IMPACTS ON OTHERS: sets the frequency to 2
+*/
+int32_t rtc_open(const uint8_t* filename){
+    int32_t temp = 2;
+    rtc_set_frequency(temp); // initialize frequency to 2 on open
+    return 0; 
+}
+
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
+    //wait until interrupt has occurred 
+    while(interrupt_flag == 0){
+    }
+    //once interrupt has occurred, reset flag and return 0
+    interrupt_flag=0; 
+    return 0;
+}
+
+int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes){
+    int32_t freq; 
+    //rtc_write must read 4 bytes, and must take in a frequency from the buffer. 
+    if (nbytes != 4 || buf == NULL){
+        return -1; 
+    }
+    freq = *((int32_t*) buf); // get frequency from buffer
+    if((freq&(freq - 1)) != 0){  //checks if frequency is a power of 2
+        return -1;
+    }
+    //sets the frequency
+    else{
+        rtc_set_frequency(freq);
+    }
+    return nbytes; 
+}
+
+/* rtc_close
+*  DESCRIPTION: Closes the RTC. 
+*  INPUTS: file descriptor
+*  OUTPUTS: NONE
+*  IMPACTS ON OTHERS: NONE
+*/
+int32_t rtc_close(int32_t fd){
+    return 0;
 }

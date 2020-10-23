13d054c0a96e6a86e1ee7cffd9bed0325bd05b75
psikor3 <psikor3@illinois.edu>
2020-10-22 20:37:24 -0500

    file functions added

--- student-distrib/FileSystem.c
+++ student-distrib/FileSystem.c
@@ -11,6 +11,106 @@
  IO: NONE
  IMPACTS ON OTHERS: 
  */
-void () {
+int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
+
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
+
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int file_open(const uint8_t* fname){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int file_close(const uint8_t* fname){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int file_write(){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int file_read(const uint8_t* fname){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int dir_open(void){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int dir_close(void){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int dir_write(void){
+    
+}
+
+/*
+ NAME: 
+ DESCRIPTION: 
+ IO: NONE
+ IMPACTS ON OTHERS: 
+ */
+int dir_read(void){
     
 }
--- student-distrib/FileSystem.h
+++ student-distrib/FileSystem.h
@@ -7,4 +7,27 @@
 
 #define example       0x1    
 
-void (void);
+typedef struct dentry {
+    char fname[32];
+    uint32_t ftype;
+    uint32_t inode;
+    uint8_t padding[24];
+} dentry_t;
+
+uint32_t curr_inodes[2][8];
+
+uint32_t boot_block_ptr;
+
+int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+
+int file_open(const uint8_t* fname);
+int file_close(const uint8_t* fname);
+int file_write();
+int file_read(const uint8_t* fname);
+
+int dir_open(void);
+int dir_close(void);
+int dir_write(void);
+int dir_read(void);

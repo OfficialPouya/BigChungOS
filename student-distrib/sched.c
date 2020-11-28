a880ecb5a5b80c20d401eb60a3996533c93b6c5f
unknown <zmahmed2@illinois.edu>
2020-11-28 06:09:43 -0600

    Got rid of some magic numbers and added to the bug log

--- student-distrib/sched.c
+++ student-distrib/sched.c
@@ -9,19 +9,20 @@ void init_PIT(uint32_t freq){
     uint8_t lower;
     uint8_t higher;
     div = MAX_PIT_FREQ / freq;
-    lower = div & 0xFF;
-    higher = (div >> 8) & 0xFF;
-    test_val = 1;
+    lower = div & 0xFF;     // get lowest byte
+    higher = (div >> 8) & 0xFF;     // get highest byte
+    test_val = 1; // used to see if pit was working
     // https://wiki.osdev.org/Programmable_Interval_Timer
-    outb(0x30 | 0x6,0x43); // x43 for mode 3, 0x30 was example given, 0x6 is
-                          // channel 0
-    outb(lower, 0x40);      // send low byte to channel 0
-    outb(higher, 0x40);  // send high byte of div to channel 0
+    outb(ACCESS_LO_HI | OP_SQUARE_WAVE, MODE_THREE_SELECT); // x43 for mode 3, 0x30 gives us access mode, 0x6 is
+                          // operating mode (square wave generator)
+    outb(lower, CHANNEL_ZERO_PORT);      // send low byte to channel 0
+    outb(higher, CHANNEL_ZERO_PORT);  // send high byte of div to channel 0
     // paul is doing the rest
 
     enable_irq(0); // PIT IRQ num is 0
     return;
 }
+
 
 /*
  NAME: pit_handler
@@ -32,7 +33,7 @@ void init_PIT(uint32_t freq){
  IMPACTS ON OTHERS: Opens New shells
  */
 void pit_handler(void){
-    if(pit_count<3){
+    if(pit_count<NUMBER_OF_TERMINALS){
         switch_terminal_work(pit_count);
         pit_count++;
         printf("Terminal %d\n", pit_count);
@@ -40,7 +41,7 @@ void pit_handler(void){
         sys_execute((uint8_t*)"shell");
     }
 
-    if (pit_count == 3){
+    if (pit_count == NUMBER_OF_TERMINALS){
         switch_terminal_work(0);
         pit_count++;
     }
@@ -99,6 +100,7 @@ void switch_terminal_work(int target_terminal){
 
     // SAVE CURR TERMINAL VALUES BACK INTO ITS PROPPER STRUCT
     // UNNECESSARY DUE TO CHANGES IN KEYBOARD, TERMINAL, AND LIB
+
     // terminals[curr_terminal].screen_x = get_screen_pos(0);
     // terminals[curr_terminal].screen_y = get_screen_pos(1);
     // terminals[curr_terminal].curr_idx = get_kb_info(0);
